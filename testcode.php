cdb9c2e191f78a4c237b4dda8969d336d27bade4
rjthakur990 <rjthakur990@gmail.com>
2019-10-10 12:58:17 +0530

    deleted unwanted file

--- testcode.php
+++
@@ -1,7 +0,0 @@
-testcode
-<script type='text/javascript'>
-    //use of cookes in javascript
-    newday = new Date();
-    document.cookie = "name=''; expires="+newday+"; path=/;";
-    document.write(document.cookie);
-</script>
